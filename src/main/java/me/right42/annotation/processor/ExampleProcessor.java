f0d54480b9df0fd39f762ad46e31e4cf5a7ef107
right42 <chjm452@naver.com>
2020-10-20 00:25:26 +0900

    annotation prcessor

--- src/main/java/me/right42/annotation/processor/ExampleProcessor.java
+++ src/main/java/me/right42/annotation/processor/ExampleProcessor.java
@@ -1,17 +1,15 @@
 package me.right42.annotation.processor;
 
 import com.google.auto.service.AutoService;
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.TypeName;
-import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.*;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
+import javax.tools.JavaFileManager;
 import java.io.IOException;
 import java.util.Set;
 
@@ -25,25 +23,40 @@ public class ExampleProcessor extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
         for (TypeElement annotation : annotations) {
-            Set<? extends Element> annotatedWith = roundEnv.getElementsAnnotatedWith(annotation);
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "process Test ");
+            Set<? extends Element> annotatedWiths = roundEnv.getElementsAnnotatedWith(annotation);
+
+            for (Element annotatedWith : annotatedWiths) {
+                try {
+                    generateJavFile(annotatedWith);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         return true;
     }
 
-    private void generateJavFile() throws IOException {
-        TypeSpec.Builder example = TypeSpec.classBuilder("Example");
-        example.addModifiers(Modifier.PUBLIC, Modifier.FINAL);
-        MethodSpec methodSpec = MethodSpec
-                            .methodBuilder("test")
-                            .addModifiers(Modifier.FINAL, Modifier.PUBLIC)
-                            .returns(TypeName.VOID)
-                            .build();
+    private void generateJavFile(Element element) throws IOException {
+        TypeSpec.Builder example = TypeSpec.classBuilder(element.getSimpleName().toString());
 
-        example.addMethod(methodSpec);
+        for (Element enclosedElement : element.getEnclosedElements()) {
+            ElementKind kind = enclosedElement.getKind();
 
-        JavaFile.builder("com.example.demo.apt", example.build())
+            ClassName className = ClassName.get(enclosedElement.asType().getClass());
+
+            if(kind.equals(ElementKind.FIELD)) {
+                MethodSpec methodSpec = MethodSpec
+                        .methodBuilder("get" + enclosedElement.getSimpleName())
+                        .addModifiers(Modifier.PUBLIC)
+                        .returns(className.getClass())
+                        .build();
+
+                example.addMethod(methodSpec);
+            }
+        }
+
+        JavaFile.builder(element.getEnclosingElement().asType().toString(), example.build())
                     .build()
                     .writeTo(processingEnv.getFiler());
     }
